b0f32e1ebd6bfbdb38887fc2c55c296bfdbd4f4e
abdulwajeedali007 <abdulwajeedali007@gmail.com>
2026-06-11 13:29:27 +0530

    mobile responsive updates

--- src/components/ProjectDetails/Index.tsx
+++ src/components/ProjectDetails/Index.tsx
@@ -1,31 +1,30 @@
 function Index() {
-    return (
-      <>
-        <div className="flex justify-between items-center my-10">
-          <div>
-            <p className="font-light font-base  ">Project</p>
-            <h3 className="font-semibold">Damac Island</h3>
-          </div>
-          <div>
-            <p className="font-light font-base">Cluster</p>
-            <h3 className="font-semibold">Cluster Island</h3>
-          </div>
-          <div>
-            <p className="font-light font-base">Country</p>
-            <h3 className="font-semibold">Dubai</h3>
-          </div>
-          <div>
-            <p className="font-light font-base">Marketing Launch Date</p>
-            <h3 className="font-semibold">12-06-2026</h3>
-          </div>
-          <div>
-            <p className="font-light font-base">Booking Launch Date</p>
-            <h3 className="font-semibold">12-06-2026</h3>
-          </div>
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4 my-10 md:flex md:justify-between md:items-center">
+        <div>
+          <p className="font-light font-base  ">Project</p>
+          <h3 className="font-semibold">Damac Island</h3>
         </div>
-      </>
-    );
-  }
-  
-  export default Index;
-  
+        <div>
+          <p className="font-light font-base">Cluster</p>
+          <h3 className="font-semibold">Cluster Island</h3>
+        </div>
+        <div>
+          <p className="font-light font-base">Country</p>
+          <h3 className="font-semibold">Dubai</h3>
+        </div>
+        <div>
+          <p className="font-light font-base">Marketing Launch Date</p>
+          <h3 className="font-semibold">12-06-2026</h3>
+        </div>
+        <div>
+          <p className="font-light font-base">Booking Launch Date</p>
+          <h3 className="font-semibold">12-06-2026</h3>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Index;
